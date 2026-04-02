f47f8eaf0ff1b49c00e8038b80dd736ae11be2df
Student <student@b426-19.intranet.cs.upt.ro>
2026-04-02 14:35:05 +0300

    Feature SM-1 (Not working yet)

--- src/main/java/loose/oose/fis/lab/student/manager/HelloApplication.java
+++ src/main/java/loose/oose/fis/lab/student/manager/HelloApplication.java
@@ -1,4 +1,4 @@
-package org.example.studentmanager;
+package loose.oose.fis.lab.student.manager;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("FIS - Student Manager");
         stage.setScene(scene);
         stage.show();
     }
